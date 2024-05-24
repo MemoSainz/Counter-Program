d4fbddec163207234e51b7275332a55aa5a53d10
MemoSainz <sainzmemo_21@proton.me>
2024-05-24 00:07:05 -0700

    A program to count from zero to infinite, in JS. // Un programa para contar de cero al infinito, en JS

---
+++ index.js
@@ -0,0 +1,23 @@
+// Counter program
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+
+const countLabel = document.getElementById("countLabel");
+
+let count = 0;
+
+increaseBtn.onclick = function () {
+  count++;
+  countLabel.textContent = count;
+};
+
+resetBtn.onclick = function () {
+  countLabel.textContent = 0;
+};
+
+decreaseBtn.onclick = function () {
+  count--;
+  countLabel.textContent = count;
+};
